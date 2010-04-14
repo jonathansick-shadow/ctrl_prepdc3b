d81e410f6231442bb9b9d894a03d145db540582d
becker <becker@git.lsstcorp.org>
2010-04-14 22:33:34 +0000

    modifying output location and 'i2' filter issue.

--- python/splitCfhtForDc3b.py
+++ python/splitCfhtForDc3b.py
@@ -10,16 +10,22 @@ import os
 import pyfits
 import numpy
 
+# where do you want the data to end up
+BASEDIR = None
+
+
 def saveScience(data, header, basedir, fieldid, visitid, filterid, snapid, ccdid, ampid):
     # CFHTLS/%(field)/raw/v%(visitid)-f%(filterid)/s%(snapid)/c%(ccdid)-a%(ampid).fits
 
-    outdir  = '%s/CFHTLS/%s/raw/v%s-f%s/s%s' % (basedir, fieldid, visitid, filterid, snapid)
+    # I think CFHTLS is redundant here, we can move this dir structure en masse into CFHTLS
+    # outdir  = '%s/CFHTLS/%s/raw/v%s-f%s/s%s' % (basedir, fieldid, visitid, filterid, snapid)
+    outdir  = '%s/%s/raw/v%s-f%s/s%s' % (basedir, fieldid, visitid, filterid, snapid)
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
         
     outfile = '%s/c%s-a%s.fits' % (outdir, ccdid, ampid)
     print '# writing', outfile
-    header.update('DC3bPATH', outfile)
+    header.update('DC3BPATH', outfile)
     pyfits.PrimaryHDU(data.astype(numpy.int16), header).writeto(outfile, output_verify='silentfix', clobber=True)
     
 def saveCalibration(data, header, basedir, dtype, dateid, ccdid, ampid, filter = None):
@@ -28,28 +34,40 @@ def saveCalibration(data, header, basedir, dtype, dateid, ccdid, ampid, filter =
     # CFHTLS/%(dtype)/v%(dateid)-f%(filterid)/c%(ccdid)-a%(ampid).fits
 
     if filter == None:
-        outdir = '%s/CFHTLS/%s/v%s' % (basedir, dtype, dateid)
+        #outdir = '%s/CFHTLS/%s/v%s' % (basedir, dtype, dateid)
+        outdir = '%s/%s/v%s' % (basedir, dtype, dateid)
     else:
-        outdir = '%s/CFHTLS/%s/v%s-f%s' % (basedir, dtype, dateid, filterid)
+        #outdir = '%s/CFHTLS/%s/v%s-f%s' % (basedir, dtype, dateid, filterid)
+        outdir = '%s/%s/v%s-f%s' % (basedir, dtype, dateid, filterid)
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     
     outfile = '%s/c%s-a%s.fits' % (outdir, ccdid, ampid)
     print '# writing', outfile
-    header.update('DC3bPATH', outfile)
+    header.update('DC3BPATH', outfile)
     pyfits.PrimaryHDU(data.astype(numpy.int16), header).writeto(outfile, output_verify='silentfix', clobber=True)
 
 
 if __name__ == '__main__':
     for file in sys.argv[1:]:
-        basedir = os.path.dirname(os.path.abspath(file))
+
+        if BASEDIR == None:
+            basedir = os.path.dirname(os.path.abspath(file))
+        else:
+            basedir = BASEDIR
+            
         ptr     = pyfits.open(file)
         head    = ptr[0].header
 
         # Some of the wides have e.g. 'w3' instead of 'W3'
         fieldid  = string.capitalize('%s' % (head['OBJECT'][:2]))
         visitid  = '%d' % (head['OBSID'])
-        filterid = '%s' % (head['FILTER'][0])   # only 1 character
+
+        filter   = head['FILTER']
+        if filter.startswith('i') and filter.endswith('2'):
+            filterid = '%s%s' % (filter[0], filter[-1])   # only 1 character
+        else:
+            filterid = '%s' % (head['FILTER'][0])   # only 1 character
         snapid   = '%02d' % (0)                 # alyways, no cosmic ray splits
 
         for ccd in range(1, len(ptr)):
@@ -74,7 +92,12 @@ if __name__ == '__main__':
             cfhtAmpA  = data[0:4644, 0:1056]
             cfhtAmpB  = data[0:4644, 1056:2112]
 
-            saveScience(cfhtAmpA, header.copy(), basedir, fieldid, visitid, filterid, snapid, '%02d' % (ccd-1), 0)
-            saveScience(cfhtAmpB, header.copy(), basedir, fieldid, visitid, filterid, snapid, '%02d' % (ccd-1), 1)
+            headerA = header.copy()
+            saveScience(cfhtAmpA, headerA, basedir, fieldid, visitid, filterid, snapid, '%02d' % (ccd-1), 0)
+            
+            headerB = header.copy()
+            crpix1 = headerB['CRPIX1']
+            headerB.update('CRPIX1', crpix1 + 1024)
+            saveScience(cfhtAmpB, headerB, basedir, fieldid, visitid, filterid, snapid, '%02d' % (ccd-1), 1)
                         
             #print data.shape, cfhtAmpA.shape, cfhtAmpB.shape
